7392341db36b5f888d92c87d50e001759f83c898
LoganSimmons98 <43660645+LoganSimmons98@users.noreply.github.com>
2018-11-09 13:33:47 -0600

    Update Events.py

--- Events.py
+++ Events.py
@@ -3,7 +3,22 @@
 
 # These are where any story events will be stored
 # Intro events, important story events and winning/ losing the game
-intro = "You are delivering a pizza to this very nice house. The man opens the door and tells you to wait while he gets the money. After about 5 minutes you hear a crashing sound and screams from another room. You go look and find the man dead in the library. You know the killer is around somewhere because you just heard the front door close and someone locking it. You need to find a way to escape or fight and win. Some items around the house will help you recover or gain energy/ability. Good luck!"
+intro = "‘Student’ goes into class early to prep for his exam, he sits at his desk inside of his\
+ classroom in Nethken Hall. Professor Andrey Timofeyev walks into the classroom like always, except\
+  he’s pulling, what looks to be a robot behind him. “Sorry for the noise class” says professor Andrey,\
+   “I’m going to work on a project while you guys take your test”. ‘Student’ decides to use the \
+   restroom before the test starts, so he gets up and goes to the restroom. When he returns, \
+   he sees a scuffle going on in the classroom. He runs into the class, the robot is going crazy\
+   , swinging his sharped edged arms around in a dangerous fit. Until one of them lands directly\
+    on professor Andrey’s head. The professor drops to the floor. The class bolts out of the \
+    classroom and runs for the doors. They start flooding out of the building ‘Student’ is \
+    almost to the front door when the robot leaps in front of him. The exit is blocked, \
+    ‘Student’ panics and pulls the fire alarm, as the alarm goes off the robot starts to \
+    go into a frenzy. ‘Student’ runs for cover. The robot swings his arms and shatters a \
+    glass door to the front office. Sparks are flying out of the robot, it starts to spin \
+    like a bottle top with its arms extended, it swings its arms right into a breaker box. \
+    The lights go out, it’s pitch black, all ‘Student’ can see is the robots glowing red eyes.\
+     ‘Student’ runs for the bathroom and hides.\n\nMAIN:A killer robot is on the loose inside of the building...‘Student’’s job is to find a way to destroy the robot or a way to escape the building...Good Luck!"
 
 
 ### Encounter Events ###
